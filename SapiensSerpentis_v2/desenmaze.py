28da0d13efa31e917778421998473e127e115ae0
qRedd <92200103+qRedd@users.noreply.github.com>
2021-10-09 18:00:28 +0300

    Add files via upload

--- SapiensSerpentis_v2/desenmaze.py
+++ SapiensSerpentis_v2/desenmaze.py
@@ -1,23 +1,24 @@
 import turtle
 
 ecran = turtle.Screen()
-ecran.bgcolor("white")
-ecran.title("The maze of LIFE")
-ecran.setup(800, 800)
+ecran.bgcolor("#0D1B2A")
+ecran.title("The Maze of LIFE")
+ecran.setup(1000, 800)
 
 
 class desen(turtle.Turtle):
     def __init__(self):
         turtle.Turtle.__init__(self)
         self.shape("square")
-        self.color("black")
+        self.color("#5777B2")
         self.penup()
         self.speed(0)
 
+levels = [""]
 
 level_1 = [
 "X  XXXXXXXXXXXXXXXXXXXXXX",
-"XP XXXXXXX          XXXXX",
+"X  XXXXXXX          XXXXX",
 "X  XXXXXXX  XXXXXX  XXXXX",
 "X       XX  XXXXXX  XXXXX",
 "X       XX  XXX        XX",
@@ -26,20 +27,46 @@ level_1 = [
 "XXXXXX  XX    XXXX  XXXXX",
 "X  XXX        XXXX  XXXXX",
 "X  XXX  XXXXXXXXXXXXXXXXX",
-"X         XXXXXXXXXXXXXXX",
+"X             XXXXXXXXXXX",
 "X                 XXXXXXX",
 "XXXXXXXXXX        XXXX  X",
 "XXXXXXXXXXXX      XXXX  X",
 "XXX  XXXXXXXX           X",
 "XXX                     X",
 "XXX       XXXXXXXXXXXXXXX",
-"XXXXXXXX  XXXXXXXXXXXXXXX",
-"XXXXXXXX                X",
-"XX   XXXXX               ",
+"XXX XXXX  XXXXXXXXXXXXXXX",
+"XXX XXXX                X",
+"XX    XXXX               ",
 "XXXXXXXXXX               ",
 "XXXXXXXXXXXXXXXXXXXXXXXXX",
 
 ]
+
+
+level_2 = [
+"XXXXXXX XXXXXXXXXXXXX",
+"X   X     X   X     X",
+"X X XXXXX XXX X X X X",
+"X X X       X X X X X",
+"X XXXXXXXXX X X XXX X",
+"X         X     X X X",
+"X X XXX X X XXX X XXX",
+"X X X X X   X X X   X",
+"X XXX X XXXXX XXXXX X",
+"X   X     X       X X",
+"X XXXXXXX XXX XXX X X",
+"X     X   X X X X   X",
+"X X XXXXXXX X X XXX X",
+"X X X   X X     X X X",
+"XXX X XXX XXXXX X XXX",
+"X   X   X   X     X X",
+"XXX X X XXX XXX XXX X",
+"X   X X X           X",
+"X X X X X XXX XXXXX X",
+"X X   X   X       X  ",
+"XXXXXXXXXXXXXXXXXXXXX",
+]
+
 
 
 def creare(level):
@@ -49,17 +76,18 @@ def creare(level):
             screen_x = -288 + (x * 24)
             screen_y = 288 - (y * 24)
             if character == "X":
-                pen.goto(screen_x, screen_y)
-                pen.stamp()
+                p.goto(screen_x, screen_y)
+                p.stamp()
            
 
-pen = desen()
+p = desen()
 
-creare(level_1)
+creare(level_2)
 
 while True:
     pass
     turtle.done()
+ 
     
 
 
